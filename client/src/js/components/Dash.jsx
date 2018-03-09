28c9d04019fc8fb402bd9ffaea8e476252bfd246
Molly Arhammar <mollyarhammar@gmail.com>
2018-03-09 08:59:38 +0100

    update no new events message

--- client/src/js/components/Dash.jsx
+++ client/src/js/components/Dash.jsx
@@ -9,7 +9,7 @@ class Dash extends Component {
       <div>
         {(this.props.name === 0) && (<h2>You are not the admin of any organizations</h2>) }
         {(this.props.name !== 0) && (<h2>Dash of {this.props.name}</h2>)}
-        {(this.props.messages.length === 0) && <p>No new events.</p>}
+        {(this.props.messages.length === 0) && <p>No new events</p>}
         {(this.props.messages.length > 0) && (this.props.messages.reverse().map((event, index) => { return (<div className='event' key={index}><p className='time'>{event.time}</p><p><span className='user'>{event.user}</span> did a <span className='type'>{event.type}</span> in <span className='repo'>{event.repo}</span></p></div>) }))}
         <Websocket
           url='ws://127.0.0.1:5050'
