663f97b8acc9515da0cdeead09317e855d8a3225
Molly Arhammar <mollyarhammar@gmail.com>
2018-03-09 16:23:51 +0100

    update service worker

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -42,8 +42,8 @@ self.addEventListener('activate', (event) => {
    */
   self.addEventListener('push', (event) => {
     console.info('Event: Push')
-    console.info(event)
-    console.log(event)
+    console.log(JSON.parse(event.data))
+    console.log(JSON.parse(event.text()))
     let payload = event.data ? event.data.text() : 'no payload'
     event.waitUntil(
       self.registration.showNotification('Shiny Github Dash', {
